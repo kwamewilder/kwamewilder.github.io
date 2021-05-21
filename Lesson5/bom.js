8a5abe18e47cb4bb7eba9d5d6ceb4d51515da974
kwamewilder <kwame.wilder@gmail.com>
2021-05-21 20:18:45 +0000

    commit

---
+++ Lesson5/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+// step5 annonymous
+button.addEventListener("click", function() {
+  let myChapter = input.value;
+  input.value = "";
+
+  const listChapter = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listChapter.appendChild(listText);
+  listText.textContent = myChapter;
+  listChapter.appendChild(listBtn);
+  listBtn.textContent = "❌";
+  list.appendChild(listChapter);
+  
+  listBtn.addEventListener("click", function () {
+  list.removeChild(listChapter);
+  })
+  
+  input.focus();
+
+
+})
