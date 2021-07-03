8ebbae2f088c684358ca54532cf02b6cd51a0a42
kwamewilder <kwame.wilder@gmail.com>
2021-07-03 11:18:52 +0000

    commit

--- Lesson11/preston/preston.js
+++ Lesson11/preston/preston.js
@@ -1,4 +1,30 @@
-const apiURL = 
+const url = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jsObject) =>{
+
+    const list = document.getElementById('listEvents');
+
+    for(let i = 0; i < jsObject.towns.length; i++){
+      
+      if (jsObject.towns[i].name == "Preston") {
+        
+        for (let j = 0; j < jsObject.towns[i].events[j].length; j++) {
+          
+          let event = document.createElement('li');
+          event.textContent = jsObject.towns[i].events[j];
+          list.appendChild(event);
+          console.log(jsObject.towns[i].events[j]);
+          
+        }
+
+      }
+      
+    }
+  });
+
+  const apiURL = 
 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=4dc63840c593a4c37e15eb58b499f77f';
 
 fetch(apiURL)
